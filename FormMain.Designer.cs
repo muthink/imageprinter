345b3cce352ad32066d04c982a46a1e34f40c9c6
Jordan Stevens <muthink@gmail.com>
2013-06-11 15:14:26 +0200

    check in working copy

---
+++ FormMain.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace imageprinter
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxImages = new System.Windows.Forms.ListBox();
+            this.comboBoxPrinters = new System.Windows.Forms.ComboBox();
+            this.buttonPrint = new System.Windows.Forms.Button();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.textBoxDirectory = new System.Windows.Forms.TextBox();
+            this.comboBoxPageSize = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.labelWidth = new System.Windows.Forms.Label();
+            this.numericUpDownWidth = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownHeight = new System.Windows.Forms.NumericUpDown();
+            this.labelHeight = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHeight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxImages
+            // 
+            this.listBoxImages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxImages.FormattingEnabled = true;
+            this.listBoxImages.Location = new System.Drawing.Point(12, 118);
+            this.listBoxImages.Name = "listBoxImages";
+            this.listBoxImages.Size = new System.Drawing.Size(318, 277);
+            this.listBoxImages.TabIndex = 0;
+            // 
+            // comboBoxPrinters
+            // 
+            this.comboBoxPrinters.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxPrinters.FormattingEnabled = true;
+            this.comboBoxPrinters.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxPrinters.Name = "comboBoxPrinters";
+            this.comboBoxPrinters.Size = new System.Drawing.Size(318, 21);
+            this.comboBoxPrinters.TabIndex = 1;
+            this.comboBoxPrinters.SelectedIndexChanged += new System.EventHandler(this.comboBoxPrinters_SelectedIndexChanged);
+            // 
+            // buttonPrint
+            // 
+            this.buttonPrint.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPrint.Location = new System.Drawing.Point(254, 405);
+            this.buttonPrint.Name = "buttonPrint";
+            this.buttonPrint.Size = new System.Drawing.Size(75, 23);
+            this.buttonPrint.TabIndex = 2;
+            this.buttonPrint.Text = "&Print";
+            this.buttonPrint.UseVisualStyleBackColor = true;
+            this.buttonPrint.Click += new System.EventHandler(this.buttonPrint_Click);
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonBrowse.Location = new System.Drawing.Point(255, 90);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 3;
+            this.buttonBrowse.Text = "Browse...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // textBoxDirectory
+            // 
+            this.textBoxDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDirectory.Location = new System.Drawing.Point(12, 92);
+            this.textBoxDirectory.Name = "textBoxDirectory";
+            this.textBoxDirectory.Size = new System.Drawing.Size(236, 20);
+            this.textBoxDirectory.TabIndex = 4;
+            // 
+            // comboBoxPageSize
+            // 
+            this.comboBoxPageSize.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxPageSize.FormattingEnabled = true;
+            this.comboBoxPageSize.Location = new System.Drawing.Point(76, 39);
+            this.comboBoxPageSize.Name = "comboBoxPageSize";
+            this.comboBoxPageSize.Size = new System.Drawing.Size(254, 21);
+            this.comboBoxPageSize.TabIndex = 5;
+            this.comboBoxPageSize.SelectedIndexChanged += new System.EventHandler(this.comboBoxPageSize_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Page Size:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonDelete.Location = new System.Drawing.Point(12, 405);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 8;
+            this.buttonDelete.Text = "&Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // labelWidth
+            // 
+            this.labelWidth.AutoSize = true;
+            this.labelWidth.Location = new System.Drawing.Point(15, 68);
+            this.labelWidth.Name = "labelWidth";
+            this.labelWidth.Size = new System.Drawing.Size(38, 13);
+            this.labelWidth.TabIndex = 9;
+            this.labelWidth.Text = "Width:";
+            // 
+            // numericUpDownWidth
+            // 
+            this.numericUpDownWidth.DecimalPlaces = 2;
+            this.numericUpDownWidth.Location = new System.Drawing.Point(59, 66);
+            this.numericUpDownWidth.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDownWidth.Name = "numericUpDownWidth";
+            this.numericUpDownWidth.Size = new System.Drawing.Size(91, 20);
+            this.numericUpDownWidth.TabIndex = 10;
+            this.numericUpDownWidth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownHeight
+            // 
+            this.numericUpDownHeight.DecimalPlaces = 2;
+            this.numericUpDownHeight.Location = new System.Drawing.Point(238, 66);
+            this.numericUpDownHeight.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDownHeight.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownHeight.Name = "numericUpDownHeight";
+            this.numericUpDownHeight.Size = new System.Drawing.Size(91, 20);
+            this.numericUpDownHeight.TabIndex = 12;
+            this.numericUpDownHeight.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // labelHeight
+            // 
+            this.labelHeight.AutoSize = true;
+            this.labelHeight.Location = new System.Drawing.Point(194, 68);
+            this.labelHeight.Name = "labelHeight";
+            this.labelHeight.Size = new System.Drawing.Size(41, 13);
+            this.labelHeight.TabIndex = 11;
+            this.labelHeight.Text = "Height:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 436);
+            this.Controls.Add(this.numericUpDownHeight);
+            this.Controls.Add(this.labelHeight);
+            this.Controls.Add(this.numericUpDownWidth);
+            this.Controls.Add(this.labelWidth);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxPageSize);
+            this.Controls.Add(this.textBoxDirectory);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.buttonPrint);
+            this.Controls.Add(this.comboBoxPrinters);
+            this.Controls.Add(this.listBoxImages);
+            this.Name = "FormMain";
+            this.Text = "Image Printer";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHeight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxImages;
+        private System.Windows.Forms.ComboBox comboBoxPrinters;
+        private System.Windows.Forms.Button buttonPrint;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.TextBox textBoxDirectory;
+        private System.Windows.Forms.ComboBox comboBoxPageSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Label labelWidth;
+        private System.Windows.Forms.NumericUpDown numericUpDownWidth;
+        private System.Windows.Forms.NumericUpDown numericUpDownHeight;
+        private System.Windows.Forms.Label labelHeight;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace imageprinter
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            comboBoxPrinters.Items.AddRange(PrinterSettings.InstalledPrinters.Cast<string>().ToArray());
+        }
+        private int _currentFile;
+        private string[] _fileList;
+        private Rectangle _pageSize;
+        private PrintDocument _pd;
+
+        private void buttonPrint_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                try
+                {
+                    _fileList = listBoxImages.Items.Cast<string>().ToArray();
+                    _pd = new PrintDocument();
+                    _pd.PrinterSettings.PrinterName = comboBoxPrinters.SelectedItem.ToString();
+                    _pd.DefaultPageSettings.PaperSize = new PaperSize("custom", (int)(numericUpDownWidth.Value*100), (int)(numericUpDownHeight.Value*100));
+                    _pageSize = _pd.DefaultPageSettings.Bounds;
+                    _pd.PrintPage += new PrintPageEventHandler(this.pd_PrintPage);
+                    _pd.Print();
+                }
+                finally
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        // The PrintPage event is raised for each page to be printed. 
+        private void pd_PrintPage(object sender, PrintPageEventArgs ev)
+        {
+            float leftMargin = ev.MarginBounds.Left;
+            float topMargin = ev.MarginBounds.Top;
+
+            Image image = Image.FromFile(_fileList[_currentFile]);
+            ev.Graphics.DrawImage(image, _pageSize);
+
+            if (++_currentFile >= _fileList.Length)
+                ev.HasMorePages = false;
+            else
+                ev.HasMorePages = true;
+        }
+
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Get files";
+            fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+            fdlg.FilterIndex = 2;
+            fdlg.Multiselect = true;
+            fdlg.CheckFileExists = true;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                listBoxImages.Items.AddRange(fdlg.FileNames);
+            }
+        }
+
+        private void fillImagesFromDirectory(string path)
+        {
+            string[] files = Directory.GetFiles(path);
+            listBoxImages.Items.AddRange(files);
+        }
+
+        private void buttonCustomPageSize_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxPrinters_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+
+            // Save the selected employee's name, because we will remove 
+            // the employee's name from the list. 
+            PrinterSettings ps = new PrinterSettings();
+            ps.PrinterName = (string)(comboBoxPrinters.SelectedItem);
+            comboBoxPageSize.Items.AddRange(ps.PaperSizes.Cast<PaperSize>().ToArray());
+        }
+
+        private void comboBoxPageSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var paperSize = (PaperSize)comboBoxPageSize.SelectedItem;
+            numericUpDownWidth.Value = paperSize.Width/(Decimal)100.0;
+            numericUpDownHeight.Value = paperSize.Height / (Decimal)100.0;
+        }
+    }
+}
